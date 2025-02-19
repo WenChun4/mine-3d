9d0869114bbc9fb04f999972677370c011f05428
WenChun4 <zwc2008ok@gmail.com>
2025-02-19 14:44:33 +0800

    Update help window

--- src/HelpWindow.tsx
+++ src/HelpWindow.tsx
@@ -36,22 +36,22 @@ export default function HelpWindow({show, setShow }: HelpWindowProps) {
             <div id='row1' className='subcontainer clearfix:after'>
               <div id='div1' className='left'>
                 <img src={help1} alt='help1 image' />
-                <Text variant='body'>Left click on a box to explore</Text>
+                <Text variant='body'><b className='highlighttext'>Left click</b> on a box to <b className='highlighttext'>explore</b></Text>
               </div>
               <div id='div2' className='right'>
                 <img src={help2} alt='help2 image' />
-                <Text variant='body'>Number 2 means 2 mines in the boxes around it, mark a mine with right click</Text>
+                <Text variant='body'>Number 2 means <b className='highlighttext'>2 mines</b> in boxes <b className='highlighttext'>around it</b>, you can <b className='highlighttext'>mark</b> a mine with <b className='highlighttext'>right click</b></Text>
               </div>
             </div>
             <Divider />
             <div id='row2' className='subcontainer clearfix:after'>
               <div id='div3' className='left'>
                 <img src={help3} alt='help3 image' />
-                <Text variant='body'>If you click on a mine by mistake, game over</Text>
+                <Text variant='body'>If you <b className='highlighttext'>click on a mine</b> by mistake, <b className='highlighttext'>game over</b></Text>
               </div>
               <div id='div4' className='right'>
                 <img src={help4} alt='help4 image' />
-                <Text variant='body'>All mines are correctly marked, game win</Text>
+                <Text variant='body'>All mines are <b className='highlighttext'>correctly marked</b>, <b className='highlighttext'>game win</b></Text>
               </div>
             </div>     
             <Divider />
@@ -59,14 +59,16 @@ export default function HelpWindow({show, setShow }: HelpWindowProps) {
             <Flex flexDirection='column' alignItems='start' gap='10px'>
               <Text variant='leading'>Source code repository:</Text>
               <Text variant='leading' className='tab'>Github repo: <a href='https://github.com/WenChun4/mine-3d' target="_blank">mine-3d</a>.</Text>
+              <p/>
               <Text variant='leading'>Referenced resources:</Text>
               <Text variant='leading' className='tab'>Generate simple 3d geometry: <a href='https://www.itwinjs.org/sandboxes/iTwinPlatform/Simple%203d/' target="_blank">Simple 3d</a>.</Text>
               <Text variant='leading' className='tab'>Generate advanced 3d geometry: <a href='https://www.itwinjs.org/sandboxes/iTwinPlatform/Advanced%203d/' target="_blank">Advanced 3d</a>.</Text>
               <Text variant='leading' className='tab'>Translate 2d geometry: <a href='https://www.itwinjs.org/sandboxes/iTwinPlatform/2d%20Transformations/' target="_blank">2d Transformations</a>.</Text>
               <Text variant='leading' className='tab'>Fire Particle Effect: <a href='https://www.itwinjs.org/sandboxes/iTwinPlatform/Fire%20Particle%20Effect/' target="_blank">Fire Particle Effect</a>.</Text>
+              <Text variant='leading' className='tab'>iTwin UI Resources: <a href='https://itwinui.bentley.com/docs' target="_blank">Document</a>.</Text>
               <Text variant='leading' className='tab'>How to implement a web backend using Express: <a href='https://chatgpt.com/' target="_blank">ChatGPT</a>.</Text>
-              <Text variant='leading' className='tab'>How to deploy react SPA with Docker: <a href='https://chatgpt.com/' target="_blank">ChatGPT</a>.</Text>
-              <Text variant='leading' className='tab'>How to host a React site: <a href='https://chatgpt.com/' target="_blank">ChatGPT</a>.</Text>
+              <Text variant='leading' className='tab'>How to deploy react SPA to Docker: <a href='https://chatgpt.com/' target="_blank">ChatGPT</a>.</Text>
+              <Text variant='leading' className='tab'>How to use public resources to host a React site: <a href='https://chatgpt.com/' target="_blank">ChatGPT</a>.</Text>
               <p/>
             </Flex>    
         </ModalContent>
